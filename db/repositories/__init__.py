92a36fb8f1499e03f0e961a699ed440f5d24d155
mark-torres10 <markptorres1@gmail.com>
2025-11-29 18:34:38 -0600

    create GeneratedBioRepository

--- db/repositories/__init__.py
+++ db/repositories/__init__.py
@@ -1,4 +1,5 @@
 from db.repositories.feed_post_repository import FeedPostRepository, SQLiteFeedPostRepository, create_sqlite_feed_post_repository
+from db.repositories.generated_bio_repository import GeneratedBioRepository, SQLiteGeneratedBioRepository, create_sqlite_generated_bio_repository
 from db.repositories.generated_feed_repository import GeneratedFeedRepository, SQLiteGeneratedFeedRepository, create_sqlite_generated_feed_repository
 from db.repositories.profile_repository import ProfileRepository, SQLiteProfileRepository, create_sqlite_profile_repository
 from db.repositories.run_repository import RunRepository, SQLiteRunRepository, create_sqlite_repository
@@ -7,6 +8,9 @@ __all__ = [
     "FeedPostRepository",
     "SQLiteFeedPostRepository",
     "create_sqlite_feed_post_repository",
+    "GeneratedBioRepository",
+    "SQLiteGeneratedBioRepository",
+    "create_sqlite_generated_bio_repository",
     "GeneratedFeedRepository",
     "SQLiteGeneratedFeedRepository",
     "create_sqlite_generated_feed_repository",
