073c22722e44b18af1c506410181e30b9df2d217
mark-torres10 <markptorres1@gmail.com>
2025-11-29 18:33:30 -0600

    Update the exports in db/adapters/__init__.py and db/adapters/sqlite/__init__.py to include the new adapter

--- db/adapters/__init__.py
+++ db/adapters/__init__.py
@@ -1,14 +1,16 @@
 """Database adapters package."""
 
-from db.adapters.base import FeedPostDatabaseAdapter, GeneratedFeedDatabaseAdapter, ProfileDatabaseAdapter, RunDatabaseAdapter
-from db.adapters.sqlite import SQLiteFeedPostAdapter, SQLiteGeneratedFeedAdapter, SQLiteProfileAdapter, SQLiteRunAdapter
+from db.adapters.base import FeedPostDatabaseAdapter, GeneratedBioDatabaseAdapter, GeneratedFeedDatabaseAdapter, ProfileDatabaseAdapter, RunDatabaseAdapter
+from db.adapters.sqlite import SQLiteFeedPostAdapter, SQLiteGeneratedBioAdapter, SQLiteGeneratedFeedAdapter, SQLiteProfileAdapter, SQLiteRunAdapter
 
 __all__ = [
     "FeedPostDatabaseAdapter",
+    "GeneratedBioDatabaseAdapter",
     "GeneratedFeedDatabaseAdapter",
     "ProfileDatabaseAdapter",
     "RunDatabaseAdapter",
     "SQLiteFeedPostAdapter",
+    "SQLiteGeneratedBioAdapter",
     "SQLiteGeneratedFeedAdapter",
     "SQLiteProfileAdapter",
     "SQLiteRunAdapter",
--- db/adapters/sqlite/__init__.py
+++ db/adapters/sqlite/__init__.py
@@ -1,12 +1,14 @@
 """SQLite adapter implementations."""
 
 from db.adapters.sqlite.feed_post_adapter import SQLiteFeedPostAdapter
+from db.adapters.sqlite.generated_bio_adapter import SQLiteGeneratedBioAdapter
 from db.adapters.sqlite.generated_feed_adapter import SQLiteGeneratedFeedAdapter
 from db.adapters.sqlite.profile_adapter import SQLiteProfileAdapter
 from db.adapters.sqlite.run_adapter import SQLiteRunAdapter
 
 __all__ = [
     "SQLiteFeedPostAdapter",
+    "SQLiteGeneratedBioAdapter",
     "SQLiteGeneratedFeedAdapter",
     "SQLiteProfileAdapter",
     "SQLiteRunAdapter",
